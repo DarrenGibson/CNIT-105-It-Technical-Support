73a0dde361c23720cb0f502937a1ec69eb1e1372
Darren Gibson <25262337+DarrenGibson@users.noreply.github.com>
2018-09-05 14:38:48 -0700

    Add files via upload

---
+++ 105Chapter3.py
@@ -0,0 +1,142 @@
+#==================================================================
+#			            IT Customer Support
+#==================================================================
+
+''' 
+Date    : Wed 05 Sep 2018 01:56:15 PM PDT 
+Subject : CNIT 105 IT Customer Support
+Title   : Chapter 3 writing for end users 
+Source  : COmputer user support, help desk and support specialists
+''' 
+
+# Chapter 3  Chapter Summary 
+
+'''
+
+User support staff are frequently assigned technical writing tasks to produce brochures,
+flyers, newsletters, handouts, traning aids, user guides, computer manuals, online help
+files, email, chat and text messages, webpages,, proposals, memos, operating procudues
+documents, and troubleshooting guides.
+
+The goal of technical weiting is to produce documents and messafes that effectively and 
+efficiantly communicate information needed by the reader.
+
+A technical writing task begins by definjing the characteristics of the targat audience
+including their bacground and reading level. Eavh document shouls be planned with 
+a cleat understaning of what the writer wants the reader to be able to do after 
+reeading it
+
+In technical documents , short words and sentences are preferable to long ones,. INformation 
+should be organized so it is easy to locate. The pourpose of a documnet and its intended 
+audience should be clearly stated so each reader can decide at the beginning whether to read 
+the document
+
+The process of technical writing unckudes prganizing ideas and topocs into an outline and 
+expanding the oputline into a first draft. Edit passes through a document are intended to 
+aliminate extra wordds, tighten rose, and checl for format consistency and technical 
+accuracy. A review bt another person is a useful check to improve a document before the funal revision.
+
+The layout of a document should help a reader inderstand the otrganication, know 
+what is important, and be aware  when transitions between topics occur. Formatting 
+should be used consistantly to enhance the information presented, not detract
+from it.
+
+Successful technical writers use strategues such as analogies, repetion, consistent word 
+use, and parallel structure ro help the audience understand the material.
+
+Common problems ro avoid in technical documents include clutter, inappropriate 
+rtpefaces, gender references, unclear referents, and passive voice. Other common 
+problems are nominalization, wordiness, jargon, undefined acronyms and initialisms, 
+idioms, and dangling phrases. 
+
+Seceral software tools, including an optline tool, spell checker, thesaurus, grammar
+checker, and destop publishing features, are available to help technical writers produce
+well-organized, accurate, and lprofessional-looking documents. 
+
+Writers use four criteria-content, organization, format and mechanics-to evaluate
+a technical document.
+
+
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
