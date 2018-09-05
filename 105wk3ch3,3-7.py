009c5134f4a4c8bfb604dad8f75b18b55c4a8444
Darren Gibson <25262337+DarrenGibson@users.noreply.github.com>
2018-09-05 15:09:26 -0700

    Add files via upload

--- 105wk3ch3,3-7.py
+++ 105wk3ch3,3-7.py
@@ -1,4 +1,11 @@
 
+
+
+'''
+
+
+
+
 Use your device without a screen 
 
 The screen-reading app, Narrator, offers simplified navagation and 
@@ -9,13 +16,38 @@ based on the reading from the book regarding
 
 
 
+
+
+		What they did right
+
 Windows for each of us
 
-THe beginnig of the document does a great job of creating an ideal Introduction 
+THe beginnig of the document does a great job of creating an ideal Introduction as clearly explained on page 113. ????
+
+This article is formatted in a way that is very readable, and a perfect
+example of how it was explained on page 118.  
+
+also there was no clutter or jargon 
+
+
+		what they could have improved on
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+'''
 
 
 
