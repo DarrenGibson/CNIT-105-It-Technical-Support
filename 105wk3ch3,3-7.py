7cc9eba6d1a7aeaac92fe4edb96c8993732da04c
Darren Gibson <25262337+DarrenGibson@users.noreply.github.com>
2018-09-05 14:58:04 -0700

    Add files via upload

---
+++ 105wk3ch3,3-7.py
@@ -0,0 +1,29 @@
+
+Use your device without a screen 
+
+The screen-reading app, Narrator, offers simplified navagation and 
+intelligent image descripion, making ir easy to explore a page without
+missing a thing on the screen.  
+
+based on the reading from the book regarding 
+
+
+
+Windows for each of us
+
+THe beginnig of the document does a great job of creating an ideal Introduction 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
